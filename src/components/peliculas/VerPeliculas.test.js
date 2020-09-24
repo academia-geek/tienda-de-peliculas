250cee748e301684694e9f11661ee43c4b2f3d2f
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-09-24 09:44:47 -0500

    cambios!

--- src/components/peliculas/VerPeliculas.test.js
+++ src/components/peliculas/VerPeliculas.test.js
@@ -3,20 +3,30 @@ import {expect} from 'chai'
 import {shallow} from 'enzyme'
 import VerPelicula from './VerPelicula'
 import { describe } from 'mocha'
+import sinon from 'sinon'
 	
 describe('/src/components/peliculas/VerPelicula.jsx', () =>  {
-    var siSaluda = false
-	const wrapper = shallow(<VerPelicula
-        saludar = {() => siSaluda = true}
-    />)
-    
-    it("validar existencia de tabla de peliculas", () => {
-        expect(wrapper.find('.tabla-peliculas').length).to.equal(1)
-    })
-    
-    it('valir onclick de boton saludar', () => {
-        const botonSaludar = wrapper.find("#btn_saludar").at(0)
-        botonSaludar.simulate('click')
-        expect(siSaluda).to.equal(true)
-    })
+	describe('carga del componente de peliculas',  () => {
+        var siSaluda = false
+        let wrapper;
+        var spy;
+        
+        beforeEach(() => {
+            spy = sinon.spy()
+            
+            wrapper = shallow(<VerPelicula
+                saludar = {spy}
+            />)
+        })
+        
+        it("validar existencia de tabla de peliculas", () => {
+            expect(wrapper.find('.tabla-peliculas').length).to.equal(1)
+        })
+        
+        it('valir onclick de boton saludar', () => {
+            const botonSaludar = wrapper.find("#btn_saludar").at(0)
+            //botonSaludar.simulate('click')
+            expect(spy.called).to.equal(true)
+        })
+    });
 })
